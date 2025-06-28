e66a985d8aca8309a3d4938d61917706ce5c51f3
Yossi Hayat <yossihayat4012@gmail.com>
2025-06-28 15:22:47 +0300

    Complete helper.py functions

--- Core-Python/LAB03-Functions-and-Modules/helper.py
+++ Core-Python/LAB03-Functions-and-Modules/helper.py
@@ -10,12 +10,22 @@ This module contains utility functions that will be imported and used in main.py
 # - Accept a parameter called 'name'
 # - Return a greeting string like "Hello, {name}!"
 
+def greet(name):
+    #name = input("Enter your name: ")
+    print(f"Hello, {name}!")
+
+greet("Yossi")
 
 # TODO: Define a function called add
 # This function should:
 # - Accept two parameters, 'x' and 'y'
 # - Return the sum of x and y
 
+def add(x, y):
+    return x + y
+
+result = add(5, 3)
+print(result)
 
 # TODO: Bonus - Define a function called max_of_three
 # This function should:
@@ -23,11 +33,23 @@ This module contains utility functions that will be imported and used in main.py
 # - Return the maximum value of the three
 # Hint: You can use the built-in max() function
 
+def max_of_three(a, b, c):
+    return max(a, b, c)
+
+result = max_of_three(1, 9, 5)
+print(result)
 
 # TODO: Extra Challenge - Create your own function
 # Define a new function that does something useful (e.g., calculate area, multiply numbers, etc.)
 # Make sure to include docstrings explaining what the function does!
 
+# The below function multiples 3 given strings by order
+# For example, parameter "3, 3, 3" will multiple 3 by 3 (9) and then multiple 9 by 3 (27)
+def multiple(a, b, c):
+    return (a * b * c)
+
+result = multiple(3, 3, 3)
+print(result)
 
 if __name__ == "__main__":
     # This code only runs when helper.py is executed directly (not when imported)
